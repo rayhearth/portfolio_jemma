09a1c05c3f0bcebbdcbab9a723c2fa68081ae357
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-01-19 16:34:14 +0100

    initialize the store with redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './assets/sass/main.scss';
 import App from './App';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from "./app/store";
 
 const pages = import.meta.glob("./pages/**/*.jsx", {eager:true})
 
@@ -43,8 +45,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+<Provider store={store}>
       <RouterProvider router={router}/>
+
+</Provider>
 
   </React.StrictMode>,
 )
