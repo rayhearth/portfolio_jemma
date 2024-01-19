e6f0060b9d89cfa020353e9b134b1bff6afc6780
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2024-01-19 18:18:35 +0100

    serach on styled component with redux

--- src/components/styled/Global.jsx
+++ src/components/styled/Global.jsx
@@ -1,12 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 
-export const GlobalStyles = createGlobalStyle`
 
+export const GlobalStyles = createGlobalStyle
+`
 body {
     background: ${({ theme }) => theme.colors.body};
     color: ${({ theme }) => theme.colors.text};
-    font - size: 1.15em;
+    font-size: 1.15em;
     margin: 0;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './assets/sass/main.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/sass/main.scss";
+import App from "./App";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./app/store";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/styled/Global";
 
-const pages = import.meta.glob("./pages/**/*.jsx", {eager:true})
+const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
 
-const routes = []
+const routes = [];
 
-for(const path of Object.keys(pages)){
+for (const path of Object.keys(pages)) {
+	const fileName = path.match(/\.\/pages\/(.*)\.jsx$/)?.[1];
+	if (!fileName) {
+		continue;
+	}
 
-  const fileName = path.match(/\.\/pages\/(.*)\.jsx$/)?.[1]
-  if(!fileName) {
-    continue
-  }
+	const normalizedPath = fileName.includes("$")
+		? fileName.replace("$", ":")
+		: fileName.replace(/\/index/, "");
 
-  const normalizedPath = fileName.includes("$")?fileName.replace('$',':'):fileName.replace(/\/index/, "")
-
-  routes.push({
-    path: fileName === "index" ? "/" : `${normalizedPath.toLocaleLowerCase()}`,
-    Element : pages[path].default,
-    loader: pages[path]?.loader,
-    action: pages[path]?.action,
-    ErrorBoundary: pages[path]?.ErrorBoundary
-})
+	routes.push({
+		path: fileName === "index" ? "/" : `${normalizedPath.toLocaleLowerCase()}`,
+		Element: pages[path].default,
+		loader: pages[path]?.loader,
+		action: pages[path]?.action,
+		ErrorBoundary: pages[path]?.ErrorBoundary,
+	});
 }
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children:
-      routes.map(({Element, ErrorBoundary, ...rest}) => ({
-        ...rest,
-        element: <Element/>,
-        ...(ErrorBoundary && {errorElement: <ErrorBoundary/>})
-      }))
-  }
-])
+	{
+		path: "/",
+		element: <App />,
+		children: routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+			...rest,
+			element: <Element />,
+			...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+		})),
+	},
+]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-<Provider store={store}>
-      <RouterProvider router={router}/>
-
-</Provider>
-
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<Provider store={store}>
+				<RouterProvider router={router} />
+			</Provider>
+		</ThemeProvider>
+	</React.StrictMode>
+);
